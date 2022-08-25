960de3c1ce1f679a707f6ce9766dafdcc89c4e54
nickgric <nick.gric@gmail.com>
2022-08-25 22:58:29 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-//Напишите функцию, которая принимает как параметр объект
-//и формирует объекты в новом массиве в формате [key, value]
-
-const user = {
-  name: "John",
-  surName: "Stones",
-  age: 20,
-  hobby: "tenis",
-  haveCar: true,
-  merried: false,
+const bookShelf = {
+  books: ["The last kingdom", "Haze", "The guardian of dreams"],
+  updateBook(oldName, newName) {
+    // Change code below this line
+    return this.indexOf(oldName, 0);
+    // Change code above this line
+  },
 };
 
-function changeObject(object) {
-  const keysValues = [];
-  const keys = Object.keys(object);
-  const values = Object.values(object);
-
-  for (let i = 0; i < keys.length; i += 1) {
-    keysValues.push([keys[i], values[i]]);
-  }
-
-  return keysValues;
-}
-
-console.log(changeObject(user));
+console.log(bookShelf.updateBook);

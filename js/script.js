7e26d7f35ee2b841b8eeba9062c5bd7e4e53d6e6
nickgric <nick.gric@gmail.com>
2022-09-03 12:12:11 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-    if (this.potions.included(newPotion)) {
-    }
-    this.potions.push(newPotion);
-  },
-  // Change code above this line
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+ .filter(user => user.gender === gender)
+ .reduce((total, balance) => total += balance, a);
 };
-
-console.log(atTheOldToad.getPotions());
